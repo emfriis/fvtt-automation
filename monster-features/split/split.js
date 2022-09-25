6b1b982d6c1991d398e29c31922c67c7708ca1e3
emfriis <emilyfriis4@gmail.com>
2022-09-25 18:12:53 +1300

    Fixed split feature to work with token effects

--- monster-features/split/split.js
+++ monster-features/split/split.js
@@ -1,15 +1,14 @@
 async function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }
 const lastArg = args[args.length - 1];
 const token = await fromUuid(lastArg.tokenUuid);
-const tactor = token?.actor ? token?.actor : token;
-if (!token || !tactor) return;
+const tactor = token.actor ? token.actor : token;
 
 let size = tactor.data.data.traits.size;
 let hp = tactor.data.data.attributes.hp.value;
 if (size === "sm" || hp < 10) {
     ui.notifications.warn("Ooze to small to split");
     return;
-}
+};
 let workflowOptions = lastArg.workflowOptions;
 let useSplit = false;
 for (let i = 0; i < workflowOptions.damageDetail.length; i++) {
@@ -22,15 +21,41 @@ for (let i = 0; i < workflowOptions.damageDetail.length; i++) {
 if (!useSplit) {
     ui.notifications.warn("No slashing or lightning damage taken");
     return;
-}
-let newSize = size === "grg" ? "huge" : size === "huge" ? "lg" : size === "lg" ? "med" : "sm";
-let updates = {
-    actor: { "data.attributes.hp.value": Math.floor(hp / 2), "data.attributes.hp.max": Math.floor(hp / 2), "data.traits.size": newSize },
-    token: { "height": Math.max(1, token.data.height - 1), "width": Math.max(1, token.data.width - 1) },
 };
-let tactorName = tactor.name;
-let center = { x: token.data.x, y: token.data.y };
-console.warn(center);
+let newSize = size === "grg" ? "huge" : size === "huge" ? "lg" : size === "lg" ? "med" : "sm";
+let newScale = newSize === "sm" ? token.data.scale * 0.75 : token.data.scale;
+let newHeight = Math.max(1, token.data.height - 1);
+let newWidth = Math.max(1, token.data.width - 1);
+let updates = {
+    token: { "height": Math.max(1, token.data.height - 1), "width": Math.max(1, token.data.width - 1) },
+    actor: { "data.attributes.hp.value": Math.floor(hp / 2), "data.attributes.hp.max": Math.floor(hp / 2), "data.traits.size": newSize },
+};
+let effectData = {
+    label: "Split Size",
+    origin: lastArg.uuid,
+    disabled: false,
+    changes: [
+        {
+            key: "ATL.width",
+            mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+            priority: 99 - newWidth,
+            value: newWidth,
+        },
+        {
+            key: "ATL.height",
+            mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+            priority: 99 - newHeight,
+            value: newHeight,
+        },
+        {
+            key: "ATL.scale",
+            mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+            priority: 99 - newHeight,
+            value: newScale,
+        },
+    ]
+};
+await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: tactor.uuid, effects: [effectData] });
+await warpgate.spawnAt({ x: token.data.x, y: token.data.y}, token.data, updates, { permanent: true });
+await warpgate.spawnAt({ x: token.data.x, y: token.data.y}, token.data, updates, { permanent: true });
 await token.delete();
-await wait(500);
-await warpgate.spawnAt(center, tactorName, updates, {}, { permanent: true, duplicates: 2 });
