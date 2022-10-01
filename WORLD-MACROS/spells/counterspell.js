c31325f8fbc3cdbb5a4b77fd5b3716e7d52851e7
emfriis <emilyfriis4@gmail.com>
2022-10-01 18:07:23 +1300

    Improved counterspell item finding

--- WORLD-MACROS/spells/counterspell.js
+++ WORLD-MACROS/spells/counterspell.js
@@ -30,15 +30,20 @@ Hooks.on("midi-qol.preambleComplete", async (workflow) => {
     if (!workflow?.token || !["spell"].includes(workflow.item.data.type)) return;
     if (workflow?.actor?.effects.find(i => i.data.label === "Metamagic: Subtle Spell") && !workflow.item.data.data?.components?.material) return;
     let counterTokens = await canvas.tokens.placeables.filter(t => {
+        let item = t?.actor.items.find(i => i.name === "Counterspell" && i.type === "spell");
         let token = (
+            item && // has item
             t?.actor && // exists
             t?.document.uuid !== workflow.token.document.uuid && // not caster
             t?.data.disposition !== workflow.token.data.disposition && // not friendly
-            t?.actor.items.find(i => i.data.name === "Counterspell" && i.data.type === "spell") && // has feature
             !t?.actor.effects.find(e => e.data.label === "Reaction" || e.data.label === "Incapacitated") && // can react
             MidiQOL.getDistance(t, workflow.token, false) <= 60 && // in range
             (game.modules.get('conditional-visibility')?.api?.canSee(t, workflow.token) && _levels?.advancedLosTestVisibility(t, workflow.token)) && // can see
-            Object.keys(t.actor.data.data.spells).find(i => (i === "pact" && t.actor.data.data.spells[i].level >= 3 && t.actor.data.data.spells[i].value > 0) || (parseInt(i.slice(-1)) >= 3 && t.actor.data.data.spells[i].value > 0)) // can cast
+            (
+                ((item.data.data.preparation?.prepared || item.data.data.preparation.mode === "always") && Object.keys(t.actor.data.data.spells).find(i => i !== "pact" && (parseInt(i.slice(-1)) >= 3 && t.actor.data.data.spells[i]?.value > 0))) ||
+                (item.data.data.preparation.mode === "pact" && t.actor.data.data.spells.pact?.level >= 3 && t.actor.data.data.spells.pact?.value > 0) || 
+                ((item.data.data.preparation.mode === "atwill" || item.data.data.preparation.mode === "innate") && (item.data.data.uses?.value > 0 || !item.data.data.uses?.max))
+            ) // can cast
         );
         return token;
     });
@@ -49,9 +54,9 @@ Hooks.on("midi-qol.preambleComplete", async (workflow) => {
         let player = await playerForActor(token?.actor);
         let socket = socketlib.registerModule("user-socket-functions");
         let useCounter = false;
-        useCounter = await socket.executeAsUser("useDialog", player.id, { title: `Counterspell`, content: `Use your reaction to cast counterspell?` });
+        useCounter = await socket.executeAsUser("useDialog", player.id, { title: `Counterspell`, content: `Use your reaction to cast Counterspell?` });
         if (useCounter) {
-            let itemUuid = token.actor.items.find(i => i.name === "Counterspell" && i.data.type === "spell")?.uuid;
+            let itemUuid = token.actor.items.find(i => i.name === "Counterspell" && i.type === "spell")?.uuid;
             let options = { targetUuids: [workflow.tokenUuid] };
             if (!itemUuid || !options) return;
             let counterCast = false;
@@ -60,14 +65,14 @@ Hooks.on("midi-qol.preambleComplete", async (workflow) => {
             if (!counterCast?.countered) {
                 let level = counterCast?.itemLevel;
                 if (level >= workflow?.itemLevel) {
-                    if (!workflow.item.name.toLowerCase().includes("counter")) return false;
+                    if (!workflow.item.name.toLowerCase().includes("counterspell")) return false;
                     workflow.countered = true;
                 } else {
                     let rollOptions = { chatMessage: true, fastForward: true };
                     let roll = await MidiQOL.socket().executeAsGM("rollAbility", { request: "abil", targetUuid: token.actor.uuid, ability: (token.actor.data.data.attributes?.spellcasting ?? "int"), options: rollOptions });
                     if (game.dice3d) game.dice3d.showForRoll(roll);
                     if (roll.total >= workflow?.itemLevel + 10) {
-                        if (!workflow.item.name.toLowerCase().includes("counter")) return false;
+                        if (!workflow.item.name.toLowerCase().includes("counterspell")) return false;
                         workflow.countered = true;
                     };
                 };
