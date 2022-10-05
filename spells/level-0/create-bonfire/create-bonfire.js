3f40691ef1b5f9e1f01a1890189b16cca0706bd2
emfriis <emilyfriis4@gmail.com>
2022-10-06 10:45:41 +1300

    Updated create bonfire aura macro

--- spells/level-0/create-bonfire/create-bonfire.js
+++ spells/level-0/create-bonfire/create-bonfire.js
@@ -1,164 +1,67 @@
+// create bonfire
+// macro.itemMacro - @details.level @details.cr @attributes.spelldc
+// aura - all, 5ft, check height, apply effect
+
 if(!game.modules.get("ActiveAuras")?.active) {
-    ui.notifications.error("ActiveAuras is not enabled");
-    return;
-  }
-  
-  const lastArg = args[args.length - 1];
-  
-  function getCantripDice(actor) {
-    const level = actor.type === "character" ? actor.data.details.level : actor.data.details.cr;
-    return 1 + Math.floor((level + 1) / 6);
-  }
-  
-  async function rollItemDamage(targetToken, itemUuid, itemLevel) {
-    const item = await fromUuid(itemUuid);
-    const caster = item.parent;
-    const ddbEffectFlags = item.data.flags.ddbimporter.effect;
-    const isCantrip = ddbEffectFlags.isCantrip;
-    const damageDice = ddbEffectFlags.dice;
-    const damageType = ddbEffectFlags.damageType;
-    const saveAbility = ddbEffectFlags.save;
-    const casterToken = canvas.tokens.placeables.find((t) => t.actor?.uuid === caster.uuid);
-    const scalingDiceArray = item.data.data.scaling.formula.split("d");
-    const scalingDiceNumber = itemLevel - item.data.data.level;
-    const upscaledDamage =  isCantrip
-      ? `${getCantripDice(caster.data)}d${scalingDiceArray[1]}[${damageType}]`
-      : scalingDiceNumber > 0 ? `${scalingDiceNumber}d${scalingDiceArray[1]}[${damageType}] + ${damageDice}` : damageDice;
-  
-    const workflowItemData = duplicate(item.data);
-    workflowItemData.data.target = { value: 1, units: "", type: "creature" };
-    workflowItemData.data.save.ability = saveAbility;
-    workflowItemData.data.components.concentration = false;
-    workflowItemData.data.level = itemLevel;
-    workflowItemData.data.duration = { value: null, units: "inst" };
-    workflowItemData.data.target = { value: null, width: null, units: "", type: "creature" };
-  
-    setProperty(workflowItemData, "flags.itemacro", {});
-    setProperty(workflowItemData, "flags.midi-qol", {});
-    setProperty(workflowItemData, "flags.dae", {});
-    setProperty(workflowItemData, "effects", []);
-    delete workflowItemData._id;
-  
-    const saveOnEntry = ddbEffectFlags.saveOnEntry;
-    console.warn("saveOnEntry", {ddbEffectFlags, saveOnEntry});
-    if (saveOnEntry) {
-      const entryItem = new CONFIG.Item.documentClass(workflowItemData, { parent: caster });
-      console.warn("Saving item on entry", {entryItem, targetToken});
-      const options = {
-        showFullCard: false,
-        createWorkflow: true,
-        targetUuids: [targetToken.document.uuid],
-        configureDialog: false,
-        versatile: false,
-        consumeResource: false,
-        consumeSlot: false,
-      };
-      await MidiQOL.completeItemRoll(entryItem, options);
-    } else {
-      const damageRoll = await new Roll(upscaledDamage).evaluate({ async: true });
-      if (game.dice3d) game.dice3d.showForRoll(damageRoll);
-  
-      workflowItemData.name = `${workflowItemData.name}: Turn Entry Damage`;
-      // console.warn("workflowItemData", workflowItemData);
-  
-      await new MidiQOL.DamageOnlyWorkflow(
-        caster,
-        casterToken.data,
-        damageRoll.total,
-        damageType,
-        [targetToken],
-        damageRoll,
-        {
-          flavor: `(${CONFIG.DND5E.damageTypes[damageType]})`,
-          itemCardId: "new",
-          itemData: workflowItemData,
-          isCritical: false,
-        }
-      );
-    }
-  
-  }
-  
-  if (args[0].tag === "OnUse" && args[0].macroPass === "preActiveEffects") {
-    const safeName = lastArg.itemData.name.replace(/\s|'|\.|’/g, "_");
-    const dataTracker = {
-      randomId: randomID(),
-      targetUuids: lastArg.targetUuids,
-      startRound: game.combat.round,
-      startTurn: game.combat.turn,
-      spellLevel: lastArg.spellLevel,
-    };
-  
-    const item = await fromUuid(lastArg.itemUuid);
-    // await item.update(dataTracker);
-    await DAE.unsetFlag(item, `${safeName}Tracker`);
-    await DAE.setFlag(item, `${safeName}Tracker`, dataTracker);
-  
-    const ddbEffectFlags = lastArg.item.flags.ddbimporter?.effect;
-    const newArgs = duplicate(args);
-    if (ddbEffectFlags) {
-      if (ddbEffectFlags.isCantrip) {
-        const cantripDice = getCantripDice(lastArg.actor);
-        newArgs[0].spellLevel = cantripDice;
-        ddbEffectFlags.cantripDice = cantripDice;
-        let newEffects = newArgs[0].item.effects.map((effect) => {
-          effect.changes = effect.changes.map((change) => {
-            change.value = change.value.replace("@cantripDice", cantripDice)
-            return change;
-          });
-          return effect;
-        });
-        newArgs[0].item.effects = duplicate(newEffects);
-        newArgs[0].itemData.effects = duplicate(newEffects);
-      }
-      const template = await fromUuid(lastArg.templateUuid);
-      await template.update({"flags.effect": ddbEffectFlags});
-    }
-  
-    return await AAhelpers.applyTemplate(newArgs);
-  
-  } else if (args[0] == "on") {
-    const safeName = lastArg.efData.label.replace(/\s|'|\.|’/g, "_");
-    const item = await fromUuid(lastArg.efData.origin);
-    const targetItemTracker = DAE.getFlag(item.parent, `${safeName}Tracker`);
-    const originalTarget = targetItemTracker.targetUuids.includes(lastArg.tokenUuid);
-    const target = canvas.tokens.get(lastArg.tokenId);
-    const targetTokenTrackerFlag = DAE.getFlag(target, `${safeName}Tracker`);
-    const targetedThisCombat = targetTokenTrackerFlag && targetItemTracker.randomId === targetTokenTrackerFlag.randomId;
-    const targetTokenTracker = targetedThisCombat
-      ? targetTokenTrackerFlag
-      : {
-        randomId: targetItemTracker.randomId,
-        round: game.combat.round,
-        turn: game.combat.turn,
-        hasLeft: false,
-      };
-  
-    const castTurn = targetItemTracker.startRound === game.combat.round && targetItemTracker.startTurn === game.combat.turn;
-    const isLaterTurn = game.combat.round > targetTokenTracker.round || game.combat.turn > targetTokenTracker.turn;
-  
-    // if:
-    // not cast turn, and not part of the original target
-    // AND one of the following
-    // not original template and have not yet had this effect applied this combat OR
-    // has been targeted this combat, left and re-entered effect, and is a later turn
-    if (castTurn && originalTarget) {
-      console.debug(`Token ${target.name} is part of the original target for ${item.name}`);
-    } else if (!targetedThisCombat || (targetedThisCombat && targetTokenTracker.hasLeft && isLaterTurn)){
-      console.debug(`Token ${target.name} is targeted for immediate damage with ${item.name}, using the following factors`, { originalTarget, castTurn, targetedThisCombat, targetTokenTracker, isLaterTurn });
-      targetTokenTracker.hasLeft = false;
-      await rollItemDamage(target, lastArg.efData.origin, targetItemTracker.spellLevel);
-    }
-    await DAE.setFlag(target, `${safeName}Tracker`, targetTokenTracker);
-  } else if (args[0] == "off") {
-    const safeName = lastArg.efData.label.replace(/\s|'|\.|’/g, "_");
-    const target = canvas.tokens.get(lastArg.tokenId);
-    const targetTokenTracker = DAE.getFlag(target, `${safeName}Tracker`);
-  
-    if (targetTokenTracker) {
-      targetTokenTracker.hasLeft = true;
-      targetTokenTracker.turn = game.combat.turn;
-      targetTokenTracker.round = game.combat.round;
-      await DAE.setFlag(target, `${safeName}Tracker`, targetTokenTracker);
-    }
-  }
+  ui.notifications.error("ActiveAuras is not enabled");
+  return;
+};
+
+const lastArg = args[args.length - 1];
+const token = canvas.tokens.get(lastArg.tokenId);
+const tokenOrActor = await fromUuid(lastArg.actorUuid);
+const tactor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
+
+async function applyDamage(sourceItem, cantripDice, saveDC, damageType, saveType) {
+  const itemData = mergeObject(
+      duplicate(sourceItem.data),
+      {
+        type: "feat",
+        effects: [],
+        flags: {
+              "midi-qol": {
+                  noProvokeReaction: true, // no reactions triggered
+                  onUseMacroName: null, // no macro
+              },
+        },
+        data: {
+              equipped: true,
+              actionType: "save",
+              save: { dc: saveDC, ability: saveType, scaling: "flat" },
+              damage: { parts: [[`${cantripDice}d8`, damageType]] },
+              "target.type": "self",
+              components: { concentration: false, material: false, ritual: false, somatic: false, value: "", vocal: false },
+              duration: { units: "inst", value: undefined },
+          },
+      },
+      { overwrite: true, inlace: true, insertKeys: true, insertValues: true }
+  );
+  setProperty(itemData.flags, "autoanimations.killAnim", true);
+  const item = new CONFIG.Item.documentClass(itemData, { parent: tactor });
+  const options = { showFullCard: false, createWorkflow: true, versatile: false, configureDialog: false };
+  await MidiQOL.completeItemRoll(item, options);
+};
+
+if (args[0].tag === "OnUse" && args[0].macroPass === "preActiveEffects") {
+  return await AAhelpers.applyTemplate(args);
+} else if (args[0] === "on") {
+  const combatTurn = game.combat.turn;
+  const efTurn = DAE.getFlag(token, lastArg.efData.id); 
+  if (combatTurn === efTurn) return;
+  const sourceItem = await fromUuid(lastArg.efData.origin);
+  const cantripDice = args[1] ? 1 + Math.floor((args[1] + 1) / 6) : 1 + Math.floor((args[2] + 1) / 6);
+  const saveDC = args[3];
+  const damageType = "fire";
+  const saveType = "dex";
+  if (!sourceItem || !cantripDice || !saveDC) return;
+  await applyDamage(sourceItem, cantripDice, saveDC, damageType, saveType);
+  await DAE.setFlag(token, lastArg.efData.id, combatTurn);
+} else if (args[0] === "each") {
+  const sourceItem = await fromUuid(lastArg.efData.origin);
+  const cantripDice = args[1] ? 1 + Math.floor((args[1] + 1) / 6) : 1 + Math.floor((args[2] + 1) / 6);
+  const saveDC = args[3];
+  const damageType = "fire";
+  const saveType = "dex";
+  if (!sourceItem || !cantripDice || !saveDC) return;
+  await applyDamage(sourceItem, cantripDice, saveDC, damageType, saveType);
+};
