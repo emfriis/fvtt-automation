7aa3e326f4ff41cc279189e76bbce882560ee4b2
emfriis <emilyfriis4@gmail.com>
2022-10-19 18:29:14 +1300

    Added advantage/disadvantage world macros

---
+++ WORLD-MACROS/conditions/proximity-disadvantage.js
@@ -0,0 +1,20 @@
+// proximity world macro
+
+Hooks.on("midi-qol.preAttackRoll", async (workflow) => {
+    if (!workflow?.token || !["mwak","rwak","msak","rsak"].includes(workflow.item.data.data.actionType)) return;
+    let negateConditions = ["Dead", "Defeated", "Incapacitated", "Stunned", "Unconcious"];
+    let nearbyEnemy = canvas.tokens.placeables.filter(t => {
+        let nearby = (
+            t?.actor &&
+            t.actor?.uuid !== workflow.actor?.uuid && // not me
+            t?.document?.uuid !== workflow.token?.document?.uuid && // not the target
+			!t.actor.effects.find(i => negateConditions.some(j => i.data.label.includes(j))) && // doesnt have negating condition
+            !(t.data.disposition === -workflow.token.data.disposition) && // not an ally
+            MidiQOL.getDistance(t, workflow.token, false) <= 5 // close to the target
+        );
+        return nearby;
+    });
+    if (nearbyEnemy.length > 0) {
+        workflow.disadvantage = true;
+    };
+});
