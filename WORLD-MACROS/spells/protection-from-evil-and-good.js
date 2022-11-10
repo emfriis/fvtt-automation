0fca99b3e37ff39695d5289a294569bd72c532b6
emfriis <emilyfriis4@gmail.com>
2022-11-10 17:53:43 +1300

    Added protection from evil and good world macro

---
+++ WORLD-MACROS/spells/protection-from-evil-and-good.js
@@ -0,0 +1,13 @@
+// protection from evil and good world macro
+
+if (!game.modules.get("midi-qol")?.active) throw new Error("requisite module(s) missing");
+
+Hooks.on("midi-qol.preAttackRoll", async (workflow) => {
+    if (!workflow?.actor || !workflow?.token || !["mwak","rwak","msak","rsak"].includes(workflow.item.data.data.actionType)) return;
+    const types = ["aberration", "celestial", "elemental", "fey", "fiends", "undead"];
+    if (!types.some(type => (workflow.actor.data.data.details?.type?.value || "").toLowerCase().includes(type) || (workflow.actor.data.data.details?.race || "").toLowerCase().includes(type))) return;
+    workflow?.targets.forEach(async (t) => {
+        if (!t?.actor || !t.actor.effects.find(i => i.data.label === "Protection from Evil and Good")) return;
+        workflow.disadvantage = true;
+    });
+});
