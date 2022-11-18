406ed441bb380d082562ae922b003eabb5d54d38
emfriis <emilyfriis4@gmail.com>
2022-11-19 07:38:43 +1300

    updated enlarge reduce macro

--- spells/level-2/enlarge-reduce/enlarge-reduce.js
+++ spells/level-2/enlarge-reduce/enlarge-reduce.js
@@ -31,17 +31,46 @@ if (args[0].tag === "OnUse" && lastArg.targetUuids.length > 0 && args[0].macroPa
 }
 
 async function reSize(flavour) {
+    const originalSizeType = tactor.data.data.traits.size;
+    const sizeTypes = {
+        grg: {
+            enlarge: "grg",
+            reduce: "huge",
+        },
+        huge: {
+            enlarge: "grg",
+            reduce: "lg",
+        },
+        lg: {
+            enlarge: "huge",
+            reduce: "med",
+        },
+        med: {
+            enlarge: "lg",
+            reduce: "sm",
+        },
+        sm: {
+            enlarge: "med",
+            reduce: "tiny",
+        },
+        tiny: {
+            enlarge: "sm",
+            reduce: "tiny",
+        },
+    }
     const originalSize = parseInt(token?.data?.width);
     const types = {
         enlarge: {
-            size: originalSize + 1,
+            size: originalSizeType === "sm" ? originalSize : originalSize + 1,
             bonus: "+1d4",
+            sizeType: sizeTypes[`${originalSizeType}`].enlarge,
         },
         reduce: {
             size: originalSize > 1 ? originalSize - 1 : originalSize - 0.3,
             bonus: "-1d4",
+            sizeType: sizeTypes[`${originalSizeType}`].reduce,
         },
-    };
+    }
     const changes = [
         {
             key: "data.bonuses.mwak.damage",
@@ -60,6 +89,12 @@ async function reSize(flavour) {
             mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
             priority: 30,
             value: `${types[flavour].size}`,
+        },
+        {
+            key: "data.traits.size",
+            mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+            priority: 20,
+            value: `${types[flavour].sizeType}`,
         },
     ];
     const effect = tactor.effects.find((e) => e.data.label === lastArg.efData.label);
--- subclass-features/tempest-cleric/wrath-of-the-storm/wrath-of-the-storm.js
+++ subclass-features/tempest-cleric/wrath-of-the-storm/wrath-of-the-storm.js
@@ -1,31 +1,29 @@
-// wrath of the storm
+// wrath of the storm WIP
 
-const tokenOrActor = await fromUuid(args[0].actorUuid);
-const tactor = tokenOrActor.actor ?? tokenOrActor;
 const token = canvas.tokens.get(args[0].tokenId);
-
-if (args[0].tag === "OnUse") {
-    let workflow = MidiQOL.Workflow.getWorkflow(args[0].workflowOptions.sourceItemUuid); 
-    if (!["mwak","rwak","msak","rsak"].includes(workflow.item.data.data.actionType) || MidiQOL.getDistance(workflow.token, token, false) > 5) return;
-    let type = await new Promise((resolve, reject) => {
-        new Dialog({
-            title: "Wrath of the Storm",
-            content: "Choose a damage type",
-            buttons: {
-                Lightning: {
-                    label: "Lightning",
-                    callback: async () => {resolve("lightning")},
-                },
-                Thunder: {
-                    label: "Thunder",
-                    callback: async () => {resolve("thunder")},
-                },
+const attackWorkflow = MidiQOL.Workflow.getWorkflow(args[0].workflowOptions.sourceItemUuid);
+console.warn(args[0].workflowOptions.sourceItemUuid);
+console.warn("WORKFLOW");
+console.warn(attackWorkflow);
+if (!["mwak","rwak","msak","rsak"].includes(attackWorkflow?.item.data.data.actionType) || MidiQOL.getDistance(workflow.token, token, false) > 5) return;
+let type = await new Promise((resolve, reject) => {
+    new Dialog({
+        title: "Wrath of the Storm",
+        content: "Choose a damage type",
+        buttons: {
+            Lightning: {
+                label: "Lightning",
+                callback: async () => {resolve("lightning")},
             },
-            default: "Lightning",
-            close: () => {resolve(false)}
-        }).render(true);
-    });
-    if (!type) return;
-    let itemWorkflow = MidiQOL.Workflow.getWorkflow(args[0].uuid); 
-    itemWorkflow.defaultDamageType = type;
-}
+            Thunder: {
+                label: "Thunder",
+                callback: async () => {resolve("thunder")},
+            },
+        },
+        default: "Lightning",
+        close: () => {resolve(false)}
+    }).render(true);
+});
+if (!type) type === "lightning";
+const itemWorkflow = MidiQOL.Workflow.getWorkflow(args[0].uuid); 
+itemWorkflow.defaultDamageType = type;
