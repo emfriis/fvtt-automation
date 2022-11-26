86b8ad680c30df56603ae63cfd7507b136198198
emfriis <emilyfriis4@gmail.com>
2022-11-27 10:11:42 +1300

    Fixed bracers of archery macro"

--- items/bracers-of-archery/bracers-of-archery.js
+++ items/bracers-of-archery/bracers-of-archery.js
@@ -6,8 +6,9 @@ const tokenOrActor = await fromUuid(lastArg.actorUuid);
 const tactor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
 
 if (args[0].tag === "DamageBonus") {
-	if (!["rwak"].includes(args[0].item.data.actionType)) return {};
-    if ((!args[0].item.data.baseItem === "longbow" && !args[0].item.data.baseItem === "shortbow") || (!args[0].item.name.includes("longbow") && !args[0].item.name.includes("shortbow"))) return {};
+	if (!["rwak"].includes(args[0].item.data.actionType)) return;
+	const items = ["longbow", "shortbow"];
+    if (!items.includes(args[0].item.data.baseItem) && !items.includes(args[0].item.name.includes("longbow"))) return;
 	const damageType = args[0].item.data.damage.parts[0][1];
 	return {damageRoll: `2[${damageType}]`, flavor: "Bracers of Archery"}
 }
