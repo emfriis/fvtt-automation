569e31b9dee90253729aba69fcbab339476d77ce
emfriis <emilyfriis4@gmail.com>
2022-11-27 09:50:27 +1300

    Updated spell resistance and incapacitated world macros

--- WORLD-MACROS/conditions/incapacitated.js
+++ WORLD-MACROS/conditions/incapacitated.js
@@ -2,11 +2,12 @@
 
 if (!game.modules.get("midi-qol")?.active) throw new Error("requisite module(s) missing");
 
-Hooks.on("midi-qol.preCheckSaves", async (workflow) => {
+Hooks.on("midi-qol.preItemRoll", async (workflow) => {
     try {
         if (!workflow?.actor || !["action", "bonus"].includes(workflow.item.data.data.activation.type)) return;
+        console.warn(workflow.actor.effects)
         if (workflow.actor.effects.find(e => ["Incapacitated", "Unconscious", "Paralyzed", "Petrified", "Stunned"].includes(e.data.label))) {
-            ui.notifications.warn(`${tactor.name} is Incapaciated`);
+            ui.notifications.warn(`${workflow.actor.name} is Incapacitated`);
             return false;
         }
     } catch(err) {
--- WORLD-MACROS/conditions/spell-resistance.js
+++ WORLD-MACROS/conditions/spell-resistance.js
@@ -4,10 +4,12 @@ if (!game.modules.get("midi-qol")?.active) throw new Error("requisite module(s) 
 
 Hooks.on("midi-qol.preCheckSaves", async (workflow) => {
     try {
-        if (!workflow?.actor || workflow?.item.type !== "spell" || workflow.item.data.data.actionType !== save) return;
-        workflow?.targets.forEach(async (t) => {
-            const tactor = t?.actor;
-            const resist = ["Spell Resilience", "Spell Resistance"]
+        if (!workflow?.actor || workflow?.item.data.type !== "spell" || workflow.item.data.data.actionType !== "save") return;
+        const targets = Array.from(workflow?.targets);
+        const resist = ["Spell Resilience", "Spell Resistance"];
+        for (let t = 0; t < targets.length; t++) {
+            let token = targets[t];
+            let tactor = token?.actor;
             if (tactor.effects.find(e => resist.includes(e.data.label)) || tactor.items.find(i => resist.includes(i.name))) {
                 const effectData = {
                     changes: [ { key: "flags.midi-qol.advantage.ability.save.all", mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, value: 1, priority: 20, } ],
@@ -17,7 +19,7 @@ Hooks.on("midi-qol.preCheckSaves", async (workflow) => {
                 }
                 await tactor.createEmbeddedDocuments("ActiveEffect", [effectData]);
             }
-        });
+        }
     } catch(err) {
         console.error(`spell resistance macro error`, err);
     }
