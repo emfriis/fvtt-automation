472889c721b70c3d94a15110691433b67e4e493d
emfriis <emilyfriis4@gmail.com>
2022-12-07 18:32:30 +1300

    Updated preAttackRoll hooks macro

--- WORLD-MACROS/hook/preAttackRoll.js
+++ WORLD-MACROS/hook/preAttackRoll.js
@@ -22,6 +22,31 @@ async function canSee(token, target) {
     return canSee;
 }
 
+async function calculateCover(sourceToken, targetToken) {
+    const sourceHeight = sourceToken.losHeight;
+    const baseZ = targetToken.data.elevation;
+    const targetHeight = targetToken.losHeight == baseZ ? baseZ+0.001 : targetToken.losHeight;
+    const sourcePov = {  x: sourceToken.center.x, y: sourceToken.center.y, z: sourceHeight, };
+    const precision = 5;
+    let volPercent = 0;
+    let collisionTestPoints = [];
+    for (let zC = baseZ; zC <= targetHeight; zC += (targetHeight - baseZ) / precision) {
+        for (let yC = targetToken.y; yC <= targetToken.y + targetToken.h; yC += targetToken.h / precision) {
+            for (let xC = targetToken.x; xC <= targetToken.x + targetToken.w; xC += targetToken.w / precision) {
+                collisionTestPoints.push({ x: xC, y: yC, z: zC });
+            }
+        }
+    }
+
+    for (let point of collisionTestPoints) {
+        if (_levels.testCollision(sourcePov, point, "sight")) volPercent++;
+    }
+
+    let calculatedCover = (volPercent * 100) / collisionTestPoints.length;
+
+    return calculatedCover;
+}
+
 Hooks.on("midi-qol.preAttackRoll", async (workflow) => {
     try {
         if (!["mwak","rwak","msak","rsak"].includes(workflow.item.data.data.actionType)) return;
@@ -31,12 +56,46 @@ Hooks.on("midi-qol.preAttackRoll", async (workflow) => {
             let tactor = token.actor;
             if (!tactor) continue;
 
-            // cover
-            try {
+		//cover
+		try {
                 console.warn("Cover activated");
-                
+                const calculatedCover = await calculateCover(workflow.token, token);
+		    if (calculatedCover >= 99) {
+		        const effectData = {
+			      changes: [
+				    { key: "data.attributes.ac.bonus", mode: CONST.ACTIVE_EFFECT_MODES.ADD, value: 9999, priority: 20, },
+				],
+            		disabled: false,
+				label: "Full Cover",
+				flags: { dae: { specialDuration: "isAttacked" } }
+			  }
+		        await tactor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+			  console.warn("Full Cover used");
+                } else if (calculatedCover >= 75) {
+		        const effectData = {
+			      changes: [
+				    { key: "data.attributes.ac.bonus", mode: CONST.ACTIVE_EFFECT_MODES.ADD, value: 5, priority: 20, },
+				],
+            		disabled: false,
+				label: "Three Quarters Cover",
+				flags: { dae: { specialDuration: "isAttacked" } }
+			  }
+		        await tactor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+			  console.warn("3/4 Cover used");
+		    } else if (calculatedCover >= 50) {
+		        const effectData = {
+			      changes: [
+			          { key: "data.attributes.ac.bonus", mode: CONST.ACTIVE_EFFECT_MODES.ADD, value: 2, priority: 20, },
+			      ],
+            	      disabled: false,
+				label: "Half Cover",
+			      flags: { dae: { specialDuration: "isAttacked" } }
+		        }
+		        await tactor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+			  console.warn("1/2 Cover used");
+		    }
             } catch (err) {
-                console.error("Cover Error", err);
+                console.error("Cover error", err);
             }
 
             // advantage
