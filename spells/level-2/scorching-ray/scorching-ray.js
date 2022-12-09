90d11bd056eceee803a476aa48c98d062c9380bf
emfriis <emilyfriis4@gmail.com>
2022-12-09 21:31:56 +1300

    Updated multi target spells

--- spells/level-2/scorching-ray/scorching-ray.js
+++ spells/level-2/scorching-ray/scorching-ray.js
@@ -1,87 +1,100 @@
 // scorching ray
 
-(async () => {
-    const lastArg = args[args.length - 1];
-    const tokenOrActor = await fromUuid(lastArg.tokenUuid);
-    const tactor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
-    const itemSource = await fromUuid(lastArg.uuid);
-    const itemCopy = mergeObject(
-        duplicate(itemSource),
-        {
-            type: "feat",
-            flags: {
-                "midi-qol": {
-                    onUseMacroName: null 
+const itemUuid = args[0].uuid;
+const attacks = args[0].spellLevel + 1;
+
+if (args[0].targets.length > attacks) {
+    return ui.notifications.error("The spell fails, You assigned more targets then you have attacks");
+}
+
+let hook = Hooks.on("midi-qol.preAttackRoll", async (workflow) => {
+
+    const targets = Array.from(workflow.targets);
+
+    if (itemUuid === workflow.uuid) {
+
+        const itemData = mergeObject(
+            duplicate(workflow.item.data),
+            {
+                type: "feat",
+                flags: {
+                    "midi-qol": {
+                        onUseMacroName: null
+                    }
+                },
+                data: {
+                    activation: {
+                        type: "none"
+                    }
                 }
             },
-            data: {
-                activation: {
-                    type: "none"
-                }
+        );
+        await workflow.actor.createEmbeddedDocuments("Item", [itemData]);
+        const attackItem = workflow.actor.items.find(i => i.name === workflow.item.name && i.data.data.activation.type === "none");
+
+        async function applyAttack(targetUuid) {
+            let rollOptions = { targetUuids: [targetUuid], showFullCard: false };
+            MidiQOL.completeItemRoll(attackItem, rollOptions);
+        };
+
+        async function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }
+
+        if (targets.length === 1) {
+            for (i = 0; i < attacks; i++) {
+                await wait(500);
+                await applyAttack(targets[0].document.uuid);
             }
-        },
-    );
-    const attackItem = new CONFIG.Item.documentClass(itemCopy, { parent: tactor });
-    const attacks = 1 + Number(lastArg.spellLevel);
-
-    async function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }
-
-    async function applyAttack(targetUuid) {
-        let rollOptions = { targetUuids: [targetUuid], showFullCard: false };
-        await MidiQOL.completeItemRoll(attackItem, rollOptions);
-    };
-
-    let workflow = await MidiQOL.Workflow.getWorkflow(lastArg.uuid);
-    await Object.assign(workflow, { targets: null, noAutoAttack: true });
-
-    if (lastArg.targets.length === 1) {
-        for (i = 0; i < attacks; i++) {
-            await wait(250);
-            await applyAttack(lastArg.targets[0].uuid);
-        }
-    } else if (lastArg.targets.length > 1) {
-        let targetContent = "";
-        lastArg.targets.forEach((t) => {
-            targetContent += `
-            <tr>
-                <td><img src="${t.data.img ?? t.img}" style="border:0px; width: 100px; height:100px;"></td>
-                <td><input type="num" id="target" min="0" max="${attacks}" name="${t.uuid}"></td>
-            </tr>
-            `;
-        });
-        let content = `<p>You have currently <b>${attacks}</b> total ${lastArg.item.name} attacks.</p><form class="flexcol"><table width="100%"><tbody><tr><th>Target</th><th>Number Attacks</th></tr>${targetContent}</tbody></table></form>`;   
-        await new Dialog({
-            title: `${lastArg.item.name}`,
-            content: content,
-            buttons: {
-                confirm: {
-                    label: "Confirm", callback: async (html) => {
-                        let spentTotal = 0;
-                        let selected_targets = html.find('input#target');
-                        for (let get_total of selected_targets) {
-                            spentTotal += Number(get_total.value);
-                        }
-                        if (spentTotal > attacks) {
-                            errorMessage = `The spell fails, You assigned more attacks then you have.`;
-                            return ui.notifications.error(errorMessage);
-                        }
-                        if (spentTotal === 0) {
-                            errorMessage = `The spell fails, No attacks spent.`;
-                            return ui.notifications.error(errorMessage);
-                        }
-                        for (let target of selected_targets) {
-                            let attackNum = Number(target.value);
-                            if (attackNum) {
-                                for (i = 0; i < attackNum; i++) {
-                                    await wait(250);
-                                    await applyAttack(target.name);
+        } else if (targets.length === attacks) {
+            for (i = 0; i < targets.length; i++) {
+                await wait(500);
+                await applyAttack(targets[i].document.uuid);
+            }
+        } else if (targets.length > 1) {
+            let targetContent = "";
+            for (i = 0; i < targets.length; i++) {
+                targetContent += `
+                <tr>
+                    <td><img src="${targets[i].data.img}" style="border:0px; width: 100px; height:100px;"></td>
+                    <td><input type="num" id="target" min="1" max="${Math.ceil(attacks / targets.length)}" name="${targets[i].document.uuid}"></td>
+                </tr>
+                `;
+            }
+            let content = `<p>You have currently <b>${attacks}</b> total ${workflow.item.name} attacks.</p><form class="flexcol"><table width="100%"><tbody><tr><th>Target</th><th>Number Attacks</th></tr>${targetContent}</tbody></table></form>`;   
+            await new Dialog({
+                title: workflow.item.name,
+                content: content,
+                buttons: {
+                    confirm: {
+                        label: "Confirm", callback: async (html) => {
+                            let attacksTotal = 0;
+                            let selectedTargets = html.find('input#target');
+                            for (let targetTotal of selectedTargets) {
+                                attacksTotal += Number(targetTotal.value);
+                            }
+                            if (attacksTotal > attacks) {
+                                return ui.notifications.error("The spell fails, You assigned more attacks then you have");
+                            } else if (attacksTotal === 0) {
+                                return ui.notifications.error("The spell fails, No attacks spent");
+                            }
+                            for (let target of selectedTargets) {
+                                let attackNum = Number(target.value);
+                                if (attackNum) {
+                                    for (i = 0; i < attackNum; i++) {
+                                        await wait(500);
+                                        console.error(target.name)
+                                        await applyAttack(target.name);
+                                    };
                                 };
                             };
-                        };
+                        },
                     },
                 },
-            },
-            default: "close"
-        }).render(true);
-    };
-})();
+                default: "close"
+            }).render(true);
+        };
+        Hooks.off("midi-qol.preAttackRoll", hook);
+        await wait(500);
+        await workflow.actor.deleteEmbeddedDocuments("Item", [attackItem.id]);
+        return false;
+    }
+});
