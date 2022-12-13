9bb418b539b1ee1c9722cf5b65a0c915fb96f3a8
emfriis <emilyfriis4@gmail.com>
2022-12-13 21:44:58 +1300

    Fixed spirit guardians macro

--- spells/level-3/spirit-guardians.js
+++ spells/level-3/spirit-guardians.js
@@ -7,8 +7,10 @@ const lastArg = args[args.length - 1];
 const tokenOrActor = await fromUuid(lastArg.actorUuid);
 const tactor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
 
+function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }
+
 // Check when applying the effect - if the token is not the caster and it IS the tokens turn they take damage
-if (args[0] === "on" && args[1] !== lastArg.tokenId && lastArg.tokenId === game.combat?.current.tokenId) {
+if (args[0] === "on" && args[1] !== lastArg.actorUuid && lastArg.tokenId === game.combat?.current.tokenId) {
   const isEvil = typeof args[5] === "string" ?  args[5]?.toLowerCase().includes("evil") : false;
   const damageDice = `${args[2]}d8`;
   const damageType = isEvil ? "necrotic" : "radiant";
@@ -16,7 +18,8 @@ if (args[0] === "on" && args[1] !== lastArg.tokenId && lastArg.tokenId === game.
   const saveType = "wis";
 
   let applyDamage = game.macros.find(m => m.name === "ApplyDamage");
-  if (applyDamage) await applyDamage.execute("ApplyDamage", lastArg.actorUuid, "self", damageDice, damageType, "magiceffect", "spelleffect", saveDC, saveType, "halfdam");
+  if (applyDamage) await applyDamage.execute("ApplyDamage", args[1], lastArg.tokenUuid, damageDice, damageType, "magiceffect", "spelleffect", saveDC, saveType, "halfdam");
   
-  await MidiQOL.socket().executeAsGM("removeEffects", { actorUuid: tactor.uuid, effects: [lastArg.efData.id] });
+  await wait (500);
+  await MidiQOL.socket().executeAsGM("removeEffects", { actorUuid: tactor.uuid, effects: [lastArg.efData._id] });
 };
