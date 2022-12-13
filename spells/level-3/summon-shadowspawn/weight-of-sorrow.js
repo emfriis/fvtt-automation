aa97e376de2c5165fe3daedcc370e2ebc03a1019
emfriis <emilyfriis4@gmail.com>
2022-12-13 21:23:57 +1300

    Fixed weight of sorrow macro

--- spells/level-3/summon-shadowspawn/weight-of-sorrow.js
+++ spells/level-3/summon-shadowspawn/weight-of-sorrow.js
@@ -3,17 +3,16 @@
 const lastArg = args[args.length - 1];
 
 if (args[0] === "each") {
-    const sourceTokenOrActor = await fromUuid(args[1]);
-    const sourceTactor = sourceTokenOrActor.actor ? sourceTokenOrActor.actor : sourceTokenOrActor;
-    const sourceParentUuid = sourceTactor.data.flags.parent;
+    const sourceItem = await fromUuid(lastArg.efData.origin)
+    const sourceActor = sourceItem.parent;
+    const sourceParentUuid = sourceActor.data.flags.parent;
     if (lastArg.actorUuid === args[1] || lastArg.actorUuid === sourceParentUuid) return;
     let effectData = {
         changes: [{ key: "data.attributes.movement.all", mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, value: "-20", priority: 20, }],
         label: "Weight of Sorrow",
         disabled: false,
         icon: "icons/creatures/magical/construct-golem-stone-blue.webp",
-        flags: { dae: { specialDuration: ["TurnStart"] } },
+        flags: { dae: { specialDuration: ["turnStart"] } },
     }
     await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: lastArg.actorUuid, effects: [effectData] });
-    await MidiQOL.socket().executeAsGM("removeEffects", { actorUuid: lastArg.actorUuid, effects: [lastArg.efData.id] });
 }
