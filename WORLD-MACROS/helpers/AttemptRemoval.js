e7a8aee4acd0e53711f87c22370fcf46a5d42e7a
emfriis <emilyfriis4@gmail.com>
2022-12-14 07:57:33 +1300

    Fixed attempt removal

--- WORLD-MACROS/helpers/AttemptRemoval.js
+++ WORLD-MACROS/helpers/AttemptRemoval.js
@@ -1,31 +1,19 @@
-// macro.itemMacro, values : dc(int) abil/save(string) type(string) auto/opt(string)
+// attempt removal macro
+// execute as gm
+// values : dc(int) abil/save(string) type(string) auto/opt(string)
 
 try {
     const lastArg = args[args.length - 1];
     const tokenOrActor = await fromUuid(lastArg.actorUuid);
     const tactor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
 
-    async function playerForActor(actor) {
+    function playerForActor(actor) {
         if (!actor) return undefined;
         let user;
         if (actor.hasPlayerOwner) user = game.users?.find(u => u.data.character === actor?.id && u.active);
         if (!user) user = game.users?.players.find(p => p.active && actor?.data.permission[p.id ?? ""] === CONST.ENTITY_PERMISSIONS.OWNER);
         if (!user) user = game.users?.find(p => p.isGM && p.active);
         return user;
-    }
-
-    async function attemptRemoval(getResist) {
-        const player = await playerForActor(tactor);
-        const rollOptions = getResist ? { chatMessage: true, fastForward: true, advantage: true } : { chatMessage: true, fastForward: true };
-        const roll = await MidiQOL.socket().executeAsUser("rollAbility", player.id, { request: args[2], targetUuid: tactor.uuid, ability: args[3], options: rollOptions }); 
-        if (game.dice3d) game.dice3d.showForRoll(roll);
-        if (roll.total >= args[1]) {
-            let ef = tactor.effects.find(i => i.data === lastArg.efData);
-            if (ef) await tactor.deleteEmbeddedDocuments("ActiveEffect", [ef.id]);
-            ChatMessage.create({ content: `The afflicted creature passes the roll and removes the ${condition} condition.` });
-        } else {
-            if (roll.total < args[1]) ChatMessage.create({ content: `The afflicted creature fails the roll and still has the ${condition} condition.` });
-        }
     }
 
     const origin = await fromUuid(lastArg.efData.origin);
@@ -73,19 +61,37 @@ try {
             resist.push("Magic Resilience", "Magic Resistance");
         }
         getResist = tactor.items.find(i => resist.includes(i.name)) || tactor.effects.find(i => resist.includes(i.data.label));
+        if (getResist) {
+            const effectData = {
+                changes: [{ key: "flags.midi-qol.advantage.ability.save.all", mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, value: 1, priority: 20, }],
+                disabled: false,
+                flags: { dae: { specialDuration: ["isSave"] } },
+                label: `Attempt Removal Save Advantage`,
+            };
+            await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: tactor.uuid, effects: [effectData] });
+        }
     }
 
     if (args[0] === "each" && lastArg.efData.disabled === false) {
+        const player = await playerForActor(tactor);
+        const saveDC = args[1];
+        const ability = args[2];
+        const type = args[3];
+        let attempt = false;
         if (args[4] === "opt") {
-            const player = await playerForActor(tactor);
             const socket = socketlib.registerModule("user-socket-functions");
-            let attempt = false;
             if (socket) attempt = await socket.executeAsUser("useDialog", player.id, { title: `Use action to attempt to remove ${condition}?`, content: `` });
-            if (attempt) {
-                attemptRemoval(getResist);
+        }
+        if (args[4] === "auto" || attempt) {
+            const roll = await MidiQOL.socket().executeAsUser("rollAbility", player.id, { request: type, targetUuid: tactor.uuid, ability: ability, options: { chatMessage: true, fastForward: true } });
+            if (game.dice3d) game.dice3d.showForRoll(roll);
+            if (roll.total >= saveDC) {
+                let ef = tactor.effects.find(i => i.data === lastArg.efData);
+                if (ef) await tactor.deleteEmbeddedDocuments("ActiveEffect", [ef.id]);
+                ChatMessage.create({ content: `The afflicted creature passes the roll and removes the ${condition} condition.` });
+            } else {
+                if (roll.total < saveDC) ChatMessage.create({ content: `The afflicted creature fails the roll and still has the ${condition} condition.` });
             }
-        } else if (args[4] === "auto") {
-            attemptRemoval(getResist);
         }
     }
 } catch (err) {
