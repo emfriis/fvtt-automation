d5ab1e18f5a93e45c46b27f8e5bceed455b6431c
emfriis <emilyfriis4@gmail.com>
2022-12-28 10:02:33 +1300

    Added banishment macro, retired polymorph macro

--- spells/level-4/banishment.js
+++ spells/level-4/banishment.js
@@ -1,18 +1,18 @@
 // banishment
 // effect itemacro
 
-// set hidden and maybe set massive negative elevation ???
-
 const lastArg = args[args.length - 1];
-const target = await fromUuid(lastArg.tokenUuid);
+const token = canvas.tokens.get(lastArg.tokenId);
+const tokenOrActor = await fromUuid(lastArg.actorUuid);
+const tactor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
 
 if (args[0] === "on") {
-  await target.update({hidden : true}); // hide targeted token
-  ChatMessage.create({content: "Target was banished"});
-
+  await tactor.setFlag("midi-qol", "banishmentElevation", token.data.elevation);
+  await canvas.scene.updateEmbeddedDocuments("Token", [{ _id: token.data._id, hidden: true, elevation: -9999 }]);
 }
 
 if (args[0]=== "off") {
-  await target.update({hidden : false}); // unhide token
-  ChatMessage.create({content: "Banished target returned"});
+  const elevation = await tactor.getFlag("midi-qol", "banishmentElevation", token.data.elevation);
+  await tactor.unsetFlag("midi-qol", "banishmentElevation");
+  await canvas.scene.updateEmbeddedDocuments("Token", [{ _id: token.data._id, hidden: false, elevation: elevation }]);
 }
