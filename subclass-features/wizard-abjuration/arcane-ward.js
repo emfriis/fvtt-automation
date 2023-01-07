ce591fde3a78b20a7504fa50d663617f39c69c1f
emfriis <emilyfriis4@gmail.com>
2023-01-07 16:40:32 +1300

    Updated arcane ward macro

--- subclass-features/wizard-abjuration/arcane-ward.js
+++ subclass-features/wizard-abjuration/arcane-ward.js
@@ -8,5 +8,5 @@ const tactor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
 if (args[0].tag === "OnUse" && lastArg.macroPass === "postActiveEffects" && lastArg.item.data.school === "abj" && lastArg.spellLevel > 0) {
 	let item = tactor.items.find(i => i.name === "Arcane Ward");
     if (!item || !item.data.data.uses.max) return;
-    await item.update({ "data.uses.value": Math.max(item.data.data.uses.max, item.data.data.uses.value + lastArg.spellLevel) });
+    await item.update({ "data.uses.value": Math.min(item.data.data.uses.max, item.data.data.uses.value + lastArg.spellLevel) });
 }
