6a470039f39fa6808b8f5dbc21657905adf51930
emfriis <emilyfriis4@gmail.com>
2023-01-07 19:22:21 +1300

    Updated life drain macro

--- monster-features/life-drain.js
+++ monster-features/life-drain.js
@@ -1,34 +1,26 @@
 // life drain
 // on use post effects
 
-if (args[0].hitTargets.length < 1 || !["mwak"].includes(args[0].itemData.data.actionType)) return;
-if (args[0].failedSaves.length < 1) return;
-token = canvas.tokens.get(args[0].tokenId);
-actor = token.actor;
-if (!actor || !token) return;
-let target = canvas.tokens.get(args[0].targets[0].id ?? args[0].targets[0]._id);
-if (!target) return;
-let targetActor = target.actor ?? target._actor;
+if (args[0].tag === "OnUse") {
+	for (let t = 0; args[0].hitTargets.length; t++) {
+		const tokenTarget = args[0].hitTargets[t];
+		const tactorTarget = tokenTarget.actor;
+		if (!tactorTarget) return;
 
-const necroticTotal = args[0].damageTotal;
-const necroticRes = (targetActor.data.data.traits.di?.value).includes("necrotic") ? 0 : (targetActor.data.data.traits.dr?.value).includes("necrotic") ? 0.5 : 1;
-const necroticVul = (targetActor.data.data.traits.dv?.value).includes("necrotic") ? 2 : 1;
-const necroticFinal = Math.floor(necroticTotal * necroticRes) * necroticVul;
+		const necroticTotal = args[0].damageTotal;
+		const necroticRes = (tactorTarget.data.data.traits.di?.value).includes("necrotic") ? 0 : (tactorTarget.data.data.traits.dr?.value).includes("necrotic") ? 0.5 : 1;
+		const necroticVul = (tactorTarget.data.data.traits.dv?.value).includes("necrotic") ? 2 : 1;
+		const necroticFinal = Math.floor(necroticTotal * necroticRes) * necroticVul;
 
-if (necroticFinal > 0) {
-	const effectData = {
-			changes: [
-				{
-					key: "data.attributes.hp.max",
-					mode: CONST.ACTIVE_EFFECT_MODES.ADD,
-					value: -necroticFinal,
-					priority: 20,
-				}
-			],
-			disabled: false,
-			flags: { dae: { specialDuration: ["longRest"] } },
-			icon: args[0].item.img,
-			label: `${args[0].item.name} Affliction`,
-	};
-	await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: targetActor.uuid, effects: [effectData] });
-};
+		if (necroticFinal > 0) {
+			const effectData = {
+				changes: [{ key: "data.attributes.hp.max", mode: CONST.ACTIVE_EFFECT_MODES.ADD, value: -necroticFinal, priority: 20, }],
+				disabled: false,
+				flags: { dae: { specialDuration: ["longRest"], stackable: "multi" } },
+				icon: args[0].item.img,
+				label: `${args[0].item.name} Affliction`,
+			};
+			await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: tactorTarget.uuid, effects: [effectData] });
+		}
+	}
+}
