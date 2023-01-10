3a22233b149e2aa7a87b5c4752d34d3e23868aaf
emfriis <emilyfriis4@gmail.com>
2023-01-10 15:17:06 +1300

    Added armor effect macros

--- WORLD-MACROS/hook/createActiveEffect.js
+++ WORLD-MACROS/hook/createActiveEffect.js
@@ -70,6 +70,36 @@ Hooks.on("createActiveEffect", async (effect) => {
             }
         }
 
+        // enable effect on armor revert creation
+        if (effect.data.changes.find(c => c.key?.includes(`flags.midi-qol.enable.armor`))) {
+            try {
+                console.warn("Enable Effect on Armor Revert Creation activated");
+                const enableTypes = effect.data.changes.filter(c => c.key?.includes(`flags.midi-qol.enable.armor`)).map(c => c.key.match(/armor.(.*)/));
+                if (!tactor.items.find(i => i.isArmor && i.data.data.equipped && (enableTypes.includes(i.data.data.armor.type) || enableTypes.includes("all")))) {
+                    await wait(100);
+                    await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: tactor.uuid, updates: [{ _id: effect.id, disabled: true }] });
+                    console.warn("Enable Effect on Armor Revert Creation used");
+                }
+            } catch (err) {
+                console.error("Enable Effect on Armor Revert Creation error", err);
+            }
+        }
+
+        // disable effect on armor creation
+        if (effect.data.changes.find(c => c.key?.includes(`flags.midi-qol.disable.armor`))) {
+            try {
+                console.warn("Disable Effect on Armor Creation activated");
+                const disableTypes = effect.data.changes.filter(c => c.key?.includes(`flags.midi-qol.disable.armor`)).map(c => c.key.match(/armor.(.*)/));
+                if (tactor.items.find(i => i.isArmor && i.data.data.equipped && (disableTypes.includes(i.data.data.armor.type) || disableTypes.includes("all")))) {
+                    await wait(100);
+                    await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: tactor.uuid, updates: [{ _id: effect.id, disabled: true }] });
+                    console.warn("Disable Effect on Armor Creation used");
+                }
+            } catch (err) {
+                console.error("Disable Effect on Armor Creation error", err);
+            }
+        }
+
     } catch(err) {
         console.error("createActiveEffect Error", err);
     }
---
+++ WORLD-MACROS/hook/updateItem.js
@@ -0,0 +1,91 @@
+// updateItem
+
+async function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }
+
+Hooks.on("updateItem", async (item) => {
+    try {
+
+        const tactor = item.parent;
+        if (!tactor) return;
+
+        // enable effect on armor
+        if (item.isArmor && item.data.data.equipped && tactor.effects.find(e => e.data.disabled && e.data.changes.find(c => c.key?.includes(`flags.midi-qol.enable.armor`)))) {
+            try {
+                console.warn("Enable Effect on Armor activated");
+                const enableIds = tactor.effects.filter(e => e.data.disabled && e.data.changes.find(c => c.key === `flags.midi-qol.enable.armor.${item.data.data.armor.type}` || c.key === `flags.midi-qol.enable.armor.all`)).map(e => e.id);
+                for (let i = 0; i < enableIds.length; i++) {
+                    await wait(100);
+                    await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: tactor.uuid, updates: [{ _id: enableIds[i], disabled: false }] });
+                }
+                console.warn("Enable Effect on Armor used");
+            } catch (err) {
+                console.error("Enable Effect on Armor error", err);
+            }
+        }
+
+        // enable effect on armor revert
+        if (item.isArmor && !item.data.data.equipped && tactor.data.flags["midi-qol"]?.enable?.armor) {
+            try {
+                console.warn("Enable Effect on Armor Revert activated");
+                if (tactor.data.flags["midi-qol"]?.enable?.armor[item.data.data.armor.type] && !tactor.items.find(i => i.isArmor && i.data.data.equipped && i.data.data.armor.type === item.data.data.armor.type)) {
+                    const enableIds = tactor.effects.filter(e => !e.data.disabled && e.data.changes.find(c => c.key === `flags.midi-qol.enable.armor.${item.data.data.armor.type}`)).map(e => e.id);
+                    for (let i = 0; i < enableIds.length; i++) {
+                        await wait(100);
+                        await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: tactor.uuid, updates: [{ _id: enableIds[i], disabled: true }] });
+                    }
+                    console.warn("Enable Effect on Armor Revert used");
+                }
+                if (tactor.data.flags["midi-qol"]?.enable?.armor?.all && !tactor.items.find(i => i.isArmor && i.data.data.equipped)) {
+                    const enableIds = tactor.effects.filter(e => !e.data.disabled && e.data.changes.find(c => c.key === `flags.midi-qol.enable.armor.all`)).map(e => e.id);
+                    for (let i = 0; i < enableIds.length; i++) {
+                        await wait(100);
+                        await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: tactor.uuid, updates: [{ _id: enableIds[i], disabled: true }] });
+                    }
+                    console.warn("Enable Effect on Armor Revert used");
+                }
+            } catch (err) {
+                console.error("Enable Effect on Armor Revert error", err);
+            }
+        }
+
+        // disable effect on armor
+        if (item.isArmor && item.data.data.equipped && (tactor.data.flags["midi-qol"]?.disable?.armor[item.data.data.armor.type] || tactor.data.flags["midi-qol"]?.disable?.armor?.all)) {
+            try {
+                console.warn("Disable Effect on Armor activated");
+                const disableIds = tactor.effects.filter(e => !e.data.disabled && e.data.changes.find(c => c.key === `flags.midi-qol.disable.armor.${item.data.data.armor.type}` || c.key === `flags.midi-qol.disable.armor.all`)).map(e => e.id);
+                for (let i = 0; i < disableIds.length; i++) {
+                    await wait(100);
+                    await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: tactor.uuid, updates: [{ _id: disableIds[i], disabled: true }] });
+                }
+                console.warn("Disable Effect on Armor used");
+            } catch (err) {
+                console.error("Disable Effect on Armor error", err);
+            }
+        }
+
+        // disable effect on armor revert
+        if (item.isArmor && !item.data.data.equipped && tactor.effects.find(e => e.data.disabled && e.data.changes.find(c => c.key?.includes(`flags.midi-qol.disable.armor`)))) {
+            try {
+                console.warn("Disable Effect on Armor Revert activated");
+                const disableIds1 = tactor.effects.filter(e => e.data.disabled && e.data.changes.find(c => c.key === `flags.midi-qol.disable.armor.${item.data.data.armor.type}`)).map(e => e.id);
+                for (let i = 0; i < disableIds1.length; i++) {
+                    await wait(100);
+                    await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: tactor.uuid, updates: [{ _id: disableIds1[i], disabled: false }] });
+                }
+                if (!tactor.items.find(i => i.isArmor && i.data.data.equipped) && tactor.effects.find(e => e.data.disabled && e.data.changes.find(c => c.key?.includes(`flags.midi-qol.disable.armor.all`)))) {
+                    const disableIds2 = tactor.effects.filter(e => e.data.disabled && e.data.changes.find(c => c.key === `flags.midi-qol.disable.armor.all`)).map(e => e.id);
+                    for (let i = 0; i < disableIds2.length; i++) {
+                        await wait(100);
+                        await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: tactor.uuid, updates: [{ _id: disableIds2[i], disabled: false }] });
+                    }
+                }
+                console.warn("Disable Effect on Armor Revert used");
+            } catch (err) {
+                console.error("Disable Effect on Armor Revert error", err);
+            }
+        }
+        
+    } catch(err) {
+        console.error("updateItem Error", err);
+    }
+});
