e12d455d3a12302e4dabd929016c354262dc433d
emfriis <emilyfriis4@gmail.com>
2023-01-11 19:23:26 +1300

    Updated elevation macro

--- WORLD-MACROS/helpers/SetTokenElevation.js
+++ WORLD-MACROS/helpers/SetTokenElevation.js
@@ -1,7 +1,7 @@
 // set token elevation
 // trigger on enter or exit drawing
 
-if (args && args.length > 1 && token && region && token.data.elevation >= (region.data.flags?.levels?.rangeBottom ?? -9999)) {
+if (args && args.length > 1 && token && region && token.data.elevation >= (region.data.flags?.levels?.rangeBottom ?? -9999) && token.data.elevation <= (region.data.flags?.levels?.rangeTop ?? 9999)) {
     let enterVal = parseFloat(args[0]);
     let exitVal = parseFloat(args[1]);
     if (event && event === MLT.ENTER && token.actor && (token.data.elevation < enterVal || token.actor.data.data.attributes.movement.fly <= 0)) {
