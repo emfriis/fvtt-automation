706dc8c3a500a755b3ebccf75c2776dfb70f4deb
emfriis <emilyfriis4@gmail.com>
2023-01-11 17:59:36 +1300

    Updated elevation macro

--- WORLD-MACROS/helpers/SetTokenElevation.js
+++ WORLD-MACROS/helpers/SetTokenElevation.js
@@ -1,17 +1,12 @@
 // set token elevation
 // trigger on enter or exit drawing
 
-if (args && args.length > 1) {
-    console.warn(region);
-    let exitval = parseFloat(args[0]);
-    let enterval = parseFloat(args[1]);
-    let elevation = 0;
-    if (event && event === MLT.ENTER) {
-        elevation = enterval;
-        if (token && token.actor && (token.data.elevation < enterval || !token.actor.data.data.attributes.movement.fly)) token.update({ "elevation": elevation });
-    } else if (event && event === MLT.LEAVE) {
-        elevation = exitval;
-        console.warn(token && token.actor && !token.actor.data.data.attributes.movement.fly)
-        if (token && token.actor && !token.actor.data.data.attributes.movement.fly) token.update({ "elevation": elevation });
+if (args && args.length > 1 && token && region && token.data.elevation >= (region.data.flags?.levels?.rangeBottom ?? -9999)) {
+    let enterVal = parseFloat(args[0]);
+    let exitVal = parseFloat(args[1]);
+    if (event && event === MLT.ENTER && token.actor && (token.data.elevation < enterVal || token.actor.data.data.attributes.movement.fly <= 0)) {
+        token.update({ "elevation": enterVal });
+    } else if (event && event === MLT.LEAVE && token.actor && token.actor.data.data.attributes.movement.fly <= 0) {
+        token.update({ "elevation": exitVal });
     }
 }
