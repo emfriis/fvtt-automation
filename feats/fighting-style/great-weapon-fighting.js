224e1d78f74ebd2d2ffa4dd5fd0aa7c3da2006a0
emfriis <emilyfriis4@gmail.com>
2023-01-15 18:54:59 +1300

    updated great weapon fighting macro

--- feats/fighting-style/great-weapon-fighting.js
+++ feats/fighting-style/great-weapon-fighting.js
@@ -8,7 +8,7 @@ if (args[0].tag === "OnUse" && args[0].macroPass === "postDamageRoll") {
   if (!["mwak"].includes(args[0].item.data.actionType) || !args[0].itemData.data.properties?.hvy) return;
   let workflow = MidiQOL.Workflow.getWorkflow(args[0].uuid); 
   let damageFormula = workflow.damageRoll.formula;
-  let newDamageFormula = damageFormula.replace(/d\d{1,2}/g, (i) => (i + "r<3"));
+  let newDamageFormula = damageFormula.replace(/d\d{1,2}/, (i) => (i + "r<3"));
   workflow.damageRoll = await new Roll(newDamageFormula).roll();
   workflow.damageTotal = workflow.damageRoll.total;
   workflow.damageRollHTML = await workflow.damageRoll.render();
