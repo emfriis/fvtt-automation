d7dc32d5969d837786d74c2d6ac3f675dfc7d661
emfriis <emilyfriis4@gmail.com>
2023-10-11 22:14:47 +1300

    Added unfinished tales from beyond macro

--- v10-updates/class-features/turn-the-x.js
+++ v10-updates/class-features/turn-the-x.js
@@ -1,17 +1,14 @@
 //turn undead
 try {
-    if (args[0].tag !== "OnUse" || args[0].macroPass !== "preambleComplete") return;
-    game.user.updateTokenTargets(args[0].targets.filter(t => ["undead"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)) || ["undead"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)).map(t => t.id)));
-} catch (err)  {console.error("Turn the Unholy Macro - ", err);}
+    if (args[0].tag == "OnUse" && args[0].macroPass == "preambleComplete") game.user.updateTokenTargets(args[0].targets.filter(t => ["undead"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)) || ["undead"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)).map(t => t.id)));
+} catch (err) {console.error("Turn the Unholy Macro - ", err);}
 
 //turn the faithless
 try {
-    if (args[0].tag !== "OnUse" || args[0].macroPass !== "preambleComplete") return;
-    game.user.updateTokenTargets(args[0].targets.filter(t => ["fey", "fiend"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)) || ["fey", "fiend"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)).map(t => t.id)));
-} catch (err)  {console.error("Turn the Unholy Macro - ", err);}
+    if (args[0].tag == "OnUse" && args[0].macroPass == "preambleComplete") game.user.updateTokenTargets(args[0].targets.filter(t => ["fey", "fiend"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)) || ["fey", "fiend"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)).map(t => t.id)));
+} catch (err) {console.error("Turn the Unholy Macro - ", err);}
 
 //turn the unholy
 try {
-    if (args[0].tag !== "OnUse" || args[0].macroPass !== "preambleComplete") return;
-    game.user.updateTokenTargets(args[0].targets.filter(t => ["undead", "fiend"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)) || ["undead", "fiend"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)).map(t => t.id)));
-} catch (err)  {console.error("Turn the Unholy Macro - ", err);}
+    if (args[0].tag == "OnUse" && args[0].macroPass == "preambleComplete") game.user.updateTokenTargets(args[0].targets.filter(t => ["undead", "fiend"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)) || ["undead", "fiend"].some(c => t.actor.system.details?.type?.value?.toLowerCase()?.includes(c)).map(t => t.id)));
+} catch (err) {console.error("Turn the Unholy Macro - ", err);}
