083deee1a0cae0fcc93f5f2bd17adfd4b4f9e78a
emfriis <emilyfriis4@gmail.com>
2023-10-14 17:10:10 +1300

    Added blinding smite and branding smite macros

---
+++ v10-updates/spells/smites/branding-smite.js
@@ -0,0 +1,35 @@
+try {
+    if (args[0].tag !== "OnUse" || args[0].macroPass !== "postActiveEffects") return;
+    const effectData = { 
+        changes: [{ key: "system.bonuses.mwak.damage", mode: 2, value: `${args[0].spellLevel}d6[radiant]`, priority: 20 }], 
+        disabled: false, 
+        icon: "icons/magic/fire/dagger-rune-enchant-flame-orange.webp", 
+        name: "Branding Smite Damage Bonus", 
+        duration: { seconds: 60 }, 
+        flags: { dae: { specialDuration: ["1Hit:mwak"] }, effectmacro: { dnd5e: { rollDamage: { script: 'try {\nworkflow = MidiQOL.Workflow.getWorkflow(this.item.uuid);\nif (workflow.item.system.actionType !== "mwak" || !workflow.hitTargets.size) return;\nconst target = workflow.hitTargets.values().next().value?.actor;\nconst effectData = {\nchanges: [{ key: "system.traits.ci.value", mode: 0, value: "invisible", priority: 20 }, { key: "ATL.light.dim", mode: 4, value: "5", priority: 20 }, { key: "ATL.light.color", mode: 5, value: "#F28C28", priority: 20 }, { key: "ATL.light.alpha", mode: 5, value: "0.1", priority: 20 }, { key: "ATL.light.animation", mode: 5, value: `{"type": "pulse", "speed": 3,"intensity": 1}`, priority: 20 }],\ndisabled: false,\nicon: "icons/magic/fire/dagger-rune-enchant-flame-orange.webp",\nname: "Branding Smite",\nduration: { seconds: 60 }\n};\nif (target) await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: target.uuid, effects: [effectData] });\nconst conc = actor.effects.find(e => e.label === "Concentrating");\nconst effect = target.effects.find(e => e.label === "Branding Smite");\nif (conc && effect) await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: actor.uuid, updates: [{ _id: conc.id, changes: conc.changes.concat([{ key: "flags.dae.deleteUuid", mode: 5, value: effect.uuid, priority: 20 }]) }] });\n} catch (err) {console.error("Branding Smite Macro - ", err)}' } } } }
+    };
+    await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: args[0].actor.uuid, effects: [effectData] });
+    const conc = args[0].actor.effects.find(e => e.label === "Concentrating");
+    const effect = args[0].actor.effects.find(e => e.label === "Branding Smite Damage Bonus");
+    if (conc && effect) await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: args[0].actor.uuid, updates: [{ _id: conc.id, changes: conc.changes.concat([{ key: `flags.dae.deleteUuid`, mode: 5, value: effect.uuid, priority: 20 }]) }] });
+} catch (err) {console.error("Branding Smite Macro - ", err)}
+
+try {
+    workflow = MidiQOL.Workflow.getWorkflow(this.item.uuid);
+    if (workflow.item.system.actionType !== "mwak" || !workflow.hitTargets.size) return;
+    const target = workflow.hitTargets.values().next().value?.actor;
+    const effectData = { 
+        changes: [{ key: "system.traits.ci.value", mode: 0, value: "invisible", priority: 20 }, { key: "ATL.light.dim", mode: 4, value: "5", priority: 20 }, { key: "ATL.light.color", mode: 5, value: "#F28C28", priority: 20 }, { key: "ATL.light.alpha", mode: 5, value: "0.1", priority: 20 }, { key: "ATL.light.animation", mode: 5, value: `{"type": "pulse", "speed": 3,"intensity": 1}`, priority: 20 }], 
+        disabled: false, 
+        icon: "icons/magic/fire/dagger-rune-enchant-flame-orange.webp", 
+        name: "Branding Smite", 
+        duration: { seconds: 60 } 
+    };
+    if (target) await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: target.uuid, effects: [effectData] });
+    const conc = actor.effects.find(e => e.label === "Concentrating");
+    const effect = target.effects.find(e => e.label === "Branding Smite");
+    if (conc && effect) await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: actor.uuid, updates: [{ _id: conc.id, changes: conc.changes.concat([{ key: `flags.dae.deleteUuid`, mode: 5, value: effect.uuid, priority: 20 }]) }] });
+} catch (err) {console.error("Branding Smite Macro - ", err)}
+
+'try {\nworkflow = MidiQOL.Workflow.getWorkflow(this.item.uuid);\nif (workflow.item.system.actionType !== "mwak" || !workflow.hitTargets.size) return;\nconst target = workflow.hitTargets.values().next().value?.actor;\nconst effectData = {\nchanges: [{ key: "system.traits.ci.value", mode: 0, value: "invisible", priority: 20 }, { key: "ATL.light.dim", mode: 4, value: "5", priority: 20 }, { key: "ATL.light.color", mode: 5, value: "#F28C28", priority: 20 }, { key: "ATL.light.alpha", mode: 5, value: "0.1", priority: 20 }, { key: "ATL.light.animation", mode: 5, value: `{"type": "pulse", "speed": 3,"intensity": 1}`, priority: 20 }],\ndisabled: false,\nicon: "icons/magic/fire/dagger-rune-enchant-flame-orange.webp",\nname: "Branding Smite",\nduration: { seconds: 60 }\n};\nif (target) await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: target.uuid, effects: [effectData] });\nconst conc = actor.effects.find(e => e.label === "Concentrating");\nconst effect = target.effects.find(e => e.label === "Branding Smite");\nif (conc && effect) await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: actor.uuid, updates: [{ _id: conc.id, changes: conc.changes.concat([{ key: "flags.dae.deleteUuid", mode: 5, value: effect.uuid, priority: 20 }]) }] });\n} catch (err) {console.error("Branding Smite Macro - ", err)}'
+
---
+++ v10-updates/spells/smites/thunderous-smite.js
@@ -0,0 +1,37 @@
+try {
+    if (args[0].tag !== "OnUse" || args[0].macroPass !== "postActiveEffects") return;
+    const effectData = { 
+        changes: [{ key: "system.bonuses.mwak.damage", mode: 2, value: "2d6[thunder]", priority: 20 }], 
+        disabled: false, 
+        icon: "icons/magic/fire/dagger-rune-enchant-orange.webp", 
+        name: "Thunderous Smite Damage Bonus", 
+        duration: { seconds: 60 }, 
+        flags: { dae: { specialDuration: ["1Hit:mwak"] }, effectmacro: { dnd5e: { rollDamage: { script: 'try {\nworkflow = MidiQOL.Workflow.getWorkflow(this.item.uuid);\nif (workflow.item.system.actionType !== "mwak" || !workflow.hitTargets.size) return;\nconst target = workflow.hitTargets.values().next().value?.actor;\nconst itemData = {\nname: "Thunderous Smite",\nimg: "icons/magic/fire/dagger-rune-enchant-orange.webp",\ntype: "feat",\nsystem: {\nactivation: { type: "special" },\ntarget: { type: "self" },\nrange: { units: "self" },\nactionType: "save",\nsave: { ability: "str", dc: `${actor.system.attributes.spelldc}`, scaling: "flat" },\n}\n}\nconst item = new CONFIG.Item.documentClass(itemData, { parent: target });\nawait MidiQOL.completeItemRoll(item, { showFullCard: false, createWorkflow: true, configureDialog: false });\n} catch (err) {console.error("Thunderous Smite Macro - ", err)}' } } } }
+    };
+    await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: args[0].actor.uuid, effects: [effectData] });
+    const conc = args[0].actor.effects.find(e => e.label === "Concentrating");
+    const effect = args[0].actor.effects.find(e => e.label === "Thunderous Smite Damage Bonus");
+    if (conc && effect) await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: args[0].actor.uuid, updates: [{ _id: conc.id, changes: conc.changes.concat([{ key: `flags.dae.deleteUuid`, mode: 5, value: effect.uuid, priority: 20 }]) }] });
+} catch (err) {console.error("Thunderous Smite Macro - ", err)}
+
+try {
+    workflow = MidiQOL.Workflow.getWorkflow(this.item.uuid);
+    if (workflow.item.system.actionType !== "mwak" || !workflow.hitTargets.size) return;
+    const target = workflow.hitTargets.values().next().value?.actor;
+    const itemData = {
+        name: "Thunderous Smite",
+        img: "icons/magic/fire/dagger-rune-enchant-orange.webp",
+        type: "feat",
+        system: {
+            activation: { type: "special" },
+            target: { type: "self" },
+            range: { units: "self" },
+            actionType: "save",
+            save: { ability: "str", dc: `${actor.system.attributes.spelldc}`, scaling: "flat" },
+        }
+    }
+    const item = new CONFIG.Item.documentClass(itemData, { parent: target });
+    await MidiQOL.completeItemRoll(item, { showFullCard: false, createWorkflow: true, configureDialog: false });
+} catch (err) {console.error("Thunderous Smite Macro - ", err)}
+
+'try {\nworkflow = MidiQOL.Workflow.getWorkflow(this.item.uuid);\nif (workflow.item.system.actionType !== "mwak" || !workflow.hitTargets.size) return;\nconst target = workflow.hitTargets.values().next().value?.actor;\nconst itemData = {\nname: "Thunderous Smite",\nimg: "icons/magic/fire/dagger-rune-enchant-orange.webp",\ntype: "feat",\nsystem: {\nactivation: { type: "special" },\ntarget: { type: "self" },\nrange: { units: "self" },\nactionType: "save",\nsave: { ability: "str", dc: `${actor.system.attributes.spelldc}`, scaling: "flat" },\n}\n}\nconst item = new CONFIG.Item.documentClass(itemData, { parent: target });\nawait MidiQOL.completeItemRoll(item, { showFullCard: false, createWorkflow: true, configureDialog: false });\n} catch (err) {console.error("Thunderous Smite Macro - ", err)}'
