7c0e8eadbb365a0b556cfe219603a5fb5478894e
emfriis <emilyfriis4@gmail.com>
2023-11-02 17:44:47 +1300

    Updated helper macros for save advantage

--- v10-updates/helper-functions/advantage-against-condition.js
+++ v10-updates/helper-functions/advantage-against-condition.js
@@ -1,4 +1,4 @@
 try {
     const statuses = [""];
-    if (args[0].tag === "OnUse" && args[0].macroPass === "preTargetSave" && args[0].workflow.item && args[0].workflow.saveDetails && (args[0].workflow.item.effects.find(e => statuses.includes(e.label.toLowerCase()) || e.changes.find(c => statuses.find(s => c.value.includes(s)))))) args[0].workflow.saveDetails.advantage = true;
+    if (args[0].tag === "OnUse" && args[0].macroPass === "preTargetSave" && args[0].workflow.item && args[0].workflow.saveDetails && (args[0].workflow.item.effects.find(e => statuses.includes(e.label.toLowerCase()) || e.changes.find(c => statuses.find(s => c.value.toLowerCase().includes(s)))))) args[0].workflow.saveDetails.advantage = true;
 } catch (err) {console.error("Status Save Advantage Macro - ", err)}
