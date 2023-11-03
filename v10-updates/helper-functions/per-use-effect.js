35c2685c35c73e87bc89f5bae5eaffdea1e61228
emfriis <emilyfriis4@gmail.com>
2023-11-04 09:08:59 +1300

    updated per use effect macro

--- v10-updates/helper-functions/per-use-effect.js
+++ v10-updates/helper-functions/per-use-effect.js
@@ -1,7 +1,7 @@
 try {
     if (args[0].tag == "OnUse" && args[0].macroPass == "preItemRoll") {
 		const actors = new Set(game.actors.contents.concat(canvas.tokens.placeables.map(t => t?.actor)).filter(a => a.effects.find(e => e.origin == args[0].item.uuid)));
-		if (actors) actors.forEach(async a => {
+		actors.forEach(async a => {
 			effects = a.effects.filter(e => e.origin == args[0].item.uuid).map(e => e.id);
 			if (effects) await MidiQOL.socket().executeAsGM("removeEffects", { actorUuid: a.uuid, effects: effects });
 		});
