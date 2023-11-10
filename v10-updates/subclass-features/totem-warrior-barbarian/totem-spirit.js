8eec944851967e8d920c1304034ddb23b3a783a0
emfriis <emilyfriis4@gmail.com>
2023-11-10 22:20:07 +1300

    Added totem barb subclass

---
+++ v10-updates/subclass-features/totem-warrior-barbarian/totem-spirit.js
@@ -0,0 +1,51 @@
+try {
+    if (args[0].tag != "OnUse" || args[0].macroPass != "preActiveEffects" || !args[0].item.effects.find(e => e.label == "Rage")) return;
+    if (args[0].actor.items.find(i => i.name == "Totem Spirit: Wolf")) {
+        let hook1 = Hooks.on("createActiveEffect", async (effect) => {
+            if (effect.label == "Rage" && effect.parent.uuid == args[0].actor.uuid) {
+                let hook2 = Hooks.on("midi-qol.RollComplete", async (workflowNext) => {
+                    if (args[0].uuid == workflowNext.uuid) {
+                        const changes = args[0].actor.effects.find(e => e.id == effect.id).changes;
+                        const effectData = {
+                            changes: [{ key: "flags.midi-qol.grants.advantage.attack.mwak", mode: 0, value: `actorUuid!="${args[0].actorUuid}"&&targetActorUuid!="${args[0].actorUuid}"`, priority: 20 }],
+                            disabled: false,
+                            label: "Totem Spirit: Wolf",
+                            icon: "icons/commodities/claws/claw-canine-brown-grey.webp",
+                            duration: { seconds: 60 },
+                            flags: { ActiveAuras: { aura: "Enemy", displayTemp: true, height: true, hidden: true, hostile: false, ignoreSelf: true, isAura: true, onlyOnce: false, radius: 5, wallsBlock: "true" } }
+                        }
+                        await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: args[0].actor.uuid, effects: [effectData] });
+                        const auraEffect = args[0].actor.effects.find(e => e.label == "Totem Spirit: Wolf");
+                        if (auraEffect) await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: args[0].actor.uuid, updates: [{ _id: effect.id, changes: changes.concat([{ key: "flags.dae.deleteUuid", mode: 5, value: auraEffect.uuid, priority: 20 }]) }] });
+                        Hooks.off("midi-qol.RollComplete", hook2);
+                    }
+                });
+                Hooks.off("createActiveEffect", hook1);
+            }
+        });
+    }
+    if (args[0].actor.items.find(i => i.name == "Totem Spirit: Bear")) {
+        let hook1 = Hooks.on("createActiveEffect", async (effect) => {
+            if (effect.label == "Rage" && effect.parent.uuid == args[0].actor.uuid) {
+                let hook2 = Hooks.on("midi-qol.RollComplete", async (workflowNext) => {
+                    if (args[0].uuid == workflowNext.uuid) {
+                        const changes = args[0].actor.effects.find(e => e.id == effect.id).changes;
+                        await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: args[0].actor.uuid, updates: [{ _id: effect.id, changes: changes.concat([
+                            { key: "system.traits.dr.value", mode: 0, value: "acid", priority: 20 },
+                            { key: "system.traits.dr.value", mode: 0, value: "cold", priority: 20 },
+                            { key: "system.traits.dr.value", mode: 0, value: "fire", priority: 20 },
+                            { key: "system.traits.dr.value", mode: 0, value: "force", priority: 20 },
+                            { key: "system.traits.dr.value", mode: 0, value: "lightning", priority: 20 },
+                            { key: "system.traits.dr.value", mode: 0, value: "necrotic", priority: 20 },
+                            { key: "system.traits.dr.value", mode: 0, value: "poison", priority: 20 },
+                            { key: "system.traits.dr.value", mode: 0, value: "radiant", priority: 20 },
+                            { key: "system.traits.dr.value", mode: 0, value: "thunder", priority: 20 }
+                        ]) }] });
+                        Hooks.off("midi-qol.RollComplete", hook2);
+                    }
+                });
+                Hooks.off("createActiveEffect", hook1);
+            }
+        });
+    }
+} catch (err) {console.error("Totem Spirit Macro - ", err)}
---
+++ v10-updates/subclass-features/totem-warrior-barbarian/totemic-attunement.js
@@ -0,0 +1,41 @@
+try {
+    if (args[0].tag != "OnUse" || args[0].macroPass != "preActiveEffects" || !args[0].item.effects.find(e => e.label == "Rage")) return;
+    if (args[0].actor.items.find(i => i.name == "Totemic Attunement: Bear")) {
+        let hook1 = Hooks.on("createActiveEffect", async (effect) => {
+            if (effect.label == "Rage" && effect.parent.uuid == args[0].actor.uuid) {
+                let hook2 = Hooks.on("midi-qol.RollComplete", async (workflowNext) => {
+                    if (args[0].uuid == workflowNext.uuid) {
+                        const changes = args[0].actor.effects.find(e => e.id == effect.id).changes;
+                        const effectData = {
+                            changes: [{ key: "flags.midi-qol.disadvantage.attack.all", mode: 0, value: `actorUuid!="${args[0].actor.uuid}"&&![...workflow.targets][0].actor.effects.find(e=>e.label=="Totemic Attunement: Bear")&&!traits.ci.value.has("frightened")&&MidiQOL.canSense(canvas.tokens.get(tokenId), canvas.tokens.get("${args[0].tokenId}"))`, priority: 20 }],
+                            disabled: false,
+                            label: "Totemic Attunement: Bear",
+                            icon: "icons/commodities/claws/claws-bear-brown-white.webp",
+                            duration: { seconds: 60 },
+                            flags: { ActiveAuras: { aura: "Enemy", displayTemp: true, height: true, hidden: true, hostile: false, ignoreSelf: true, isAura: true, nameOverride: "Totemic Attunement: Bear Disadvantage", onlyOnce: false, radius: 5, wallsBlock: "true" } }
+                        }
+                        await MidiQOL.socket().executeAsGM("createEffects", { actorUuid: args[0].actor.uuid, effects: [effectData] });
+                        const auraEffect = args[0].actor.effects.find(e => e.label == "Totemic Attunement: Bear");
+                        if (auraEffect) await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: args[0].actor.uuid, updates: [{ _id: effect.id, changes: changes.concat([{ key: "flags.dae.deleteUuid", mode: 5, value: auraEffect.uuid, priority: 20 }]) }] });
+                        Hooks.off("midi-qol.RollComplete", hook2);
+                    }
+                });
+                Hooks.off("createActiveEffect", hook1);
+            }
+        });
+    }
+    if (args[0].actor.items.find(i => i.name == "Totemic Attunement: Eagle")) {
+        let hook1 = Hooks.on("createActiveEffect", async (effect) => {
+            if (effect.label == "Rage" && effect.parent.uuid == args[0].actor.uuid) {
+                let hook2 = Hooks.on("midi-qol.RollComplete", async (workflowNext) => {
+                    if (args[0].uuid == workflowNext.uuid) {
+                        const changes = args[0].actor.effects.find(e => e.id == effect.id).changes;
+                        await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: args[0].actor.uuid, updates: [{ _id: effect.id, changes: changes.concat([{ key: "system.attributes.movement.fly", mode: 4, value: `${args[0].actor.system.attributes.movement.walk}`, priority: 20 }]) }] });
+                        Hooks.off("midi-qol.RollComplete", hook2);
+                    }
+                });
+                Hooks.off("createActiveEffect", hook1);
+            }
+        });
+    }
+} catch (err) {console.error("Totemic Attunement Macro - ", err)}
