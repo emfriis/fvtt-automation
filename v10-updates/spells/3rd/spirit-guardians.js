d5738f4c3ffc9ec8f525801d7a84f254f6f52b3a
emfriis <emilyfriis4@gmail.com>
2023-11-12 13:36:43 +1300

    Added Spirit Guardians Macro

---
+++ v10-updates/spells/3rd/spirit-guardians.js
@@ -0,0 +1,37 @@
+try {
+    const lastArg = args[args.length - 1];
+    if (lastArg.tag == "OnUse" && lastArg.macroPass == "preActiveEffects" && lastArg.actor.system.details.alignment.toLowerCase().includes("evil") && lastArg.item.effects.find(e => e.label == "Spirit Guardians")) {
+        let hook1 = Hooks.on("createActiveEffect", async (effect) => {
+            if (effect.label == "Spirit Guardians" && effect.parent.uuid == lastArg.actor.uuid) {
+                let hook2 = Hooks.on("midi-qol.RollComplete", async (workflowNext) => {
+                    if (lastArg.uuid == workflowNext.uuid) {
+                        let changes = lastArg.actor.effects.find(e => e.id == effect.id).changes;
+                        changes.forEach(c => { c.value = c.value.replace("radiant", "necrotic"); });
+                        await MidiQOL.socket().executeAsGM("updateEffects", { actorUuid: lastArg.actor.uuid, updates: [{ _id: effect.id, changes: changes }] });
+                        Hooks.off("midi-qol.RollComplete", hook2);
+                    }
+                });
+                Hooks.off("createActiveEffect", hook1);
+            }
+        });
+    } else if (args[0] == "on" && !isNaN(args[1]) && !isNaN(args[3]) && !lastArg.efData.origin.includes(lastArg.actorUuid) && lastArg.tokenId == game.combat?.current?.tokenId) {
+        const tokenOrActor = await fromUuid(lastArg.actorUuid);
+        const actor = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
+        const source = game.actors.get(lastArg.efData.origin.match(/Actor\.(.*?)\./)[1]) ?? canvas.tokens.placeables.find(t => t.actor && t.actor.id == lastArg.efData.origin.match(/Actor\.(.*?)\./)[1])?.actor;
+        const itemData = {
+            name: "Spiritual Guardians",
+            img: "icons/magic/light/projectile-bolts-salvo-white.webp",
+            type: "feat",
+            flags: { midiProperties: { magiceffect: true, halfdam: true }, autoanimations: { isEnabled: false } },
+            system: {
+                activation: { type: "special" },
+                target: { type: "creature" },
+                actionType: "save",
+                damage: { parts: [[`${args[1]}d8`, args[2]]] },
+                save: { ability: "wis", dc: args[3], scaling: "flat" }
+            }
+        }
+        const item = new CONFIG.Item.documentClass(itemData, { parent: source ?? actor });
+        await MidiQOL.completeItemRoll(item, { showFullCard: true, createWorkflow: true, configureDialog: false, targetUuids: [lastArg.tokenUuid] });
+    }
+} catch (err) {console.error("Spirit Guardians Macro - ", err)}
